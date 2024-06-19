ca93e647fd5f39243a3c7552ca3dee6d4572908f
DouglasGavioli <douglasbgavioli@hotmail.com>
2024-06-19 17:38:13 -0300

    Integrando com hygraph, mas com erro

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -20,7 +20,7 @@ export const Header = () => {
         <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center ">
             <div className="container flex items-center justify-between">
                 <Link href="/">
-                    {/* <Image width={58} height={49} src="/images/logo-dg.png" alt={"Logo portfolio"} /> */}
+                    <Image width={58} height={49} src="/images/logo-dg.png" alt={"Logo portfolio"} />
 
                 </Link>
                 <nav className="flex itens-center gap-4">
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,44 @@ import { HeroSection } from "./components/pages/home/hero-section";
 import { HighlightedProjects } from "./components/pages/home/highlighted-projects";
 import { KnowTechs } from "./components/pages/home/known-techs";
 import { WorkExperience } from "./components/pages/home/work-experience";
+import { HomePageData } from "./types/page-info";
+import { fetchHygraphQuery } from "./utils/fetch-hygraph-query";
+
+const getPageData = async (): Promise<HomePageData> => {
+  const query = `
+  query PageInfoQuery {
+    page(where: {slug: "home"}) {
+      introduction {
+        raw
+      }
+      technologies {
+        name
+      }
+      profilePicture {
+        url
+      }
+      socials {
+        url
+        iconSvg
+      }
+      knownTechs {
+        iconSvg
+        name
+        startDate
+      }
+      
+    }
+
+  }
+`
+  return fetchHygraphQuery(
+    query
+  )
+}
 
 export default async function Home() {
+  const { page } = await getPageData();
+
   return (
     <>
       <HeroSection />
