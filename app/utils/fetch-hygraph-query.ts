9aa29d54a6581790d5f813998fad81cfcbf265d2
DouglasGavioli <douglasbgavioli@hotmail.com>
2024-07-11 10:54:00 -0300

    validacao da api e remocao de um dos eslints

--- app/utils/fetch-hygraph-query.ts
+++ app/utils/fetch-hygraph-query.ts
@@ -1,18 +1,26 @@
-export const fetchHygraphQuery = async (query: string, revalidate?: number)=>{
-    const response = await fetch(process.env.HYGRAPH_URL!,{
+export const fetchHygraphQuery = async (query: string, revalidate?: number) => {
+    const response = await fetch(process.env.HYGRAPH_URL!, {
         method: "POST",
-        headers:{
+        headers: {
             'Content-Type': 'application/json',
             Accept: 'application/json',
             Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`
         },
-        body: JSON.stringify({query}),
+        body: JSON.stringify({ query }),
         next: {
             revalidate
         }
-    })
-    
-    const {data} = await response.json()
-    
-    return data
-}
+    });
+
+    if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
+    const json = await response.json();
+
+    if (!json || !json.data) {
+        throw new Error('No data returned from API');
+    }
+
+    return json.data;
+};
