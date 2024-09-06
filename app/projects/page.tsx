1f369fd0217937025319138edc0d638d9cc2f757
DouglasGavioli <douglasbgavioli@hotmail.com>
2024-09-06 18:25:15 -0300

    teste

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -26,7 +26,7 @@ const getPageData = async (): Promise<ProjectsPageData> => {
 
   return fetchHygraphQuery(
     query,
-    1000 * 60 * 60 * 24, // 1 day
+    // 1000 * 60 * 60 * 24, // 1 day
   )
 }
 
